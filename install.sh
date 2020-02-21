c72ace56dc5774b029df6aaa9b02f82fb494654c
Matheus sampaio Queiroga <50121801+Sirherobrine23@users.noreply.github.com>
2020-02-21 08:12:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,17 +20,17 @@ echo "Installing Samba";
 echo "Config samba";
   cd /tmp > /dev/null 2>&1;
   wget https://raw.githubusercontent.com/Sirherobrine23/web-core/master/smb.txt > /dev/null 2>&1;
-  rm /etc/samba/smb.conf > /dev/null 2>&1;
-  cat /tmp/smb.txt >> /etc/samba/smb.conf ;
-  service smbd reload > /dev/null 2>&1;
+  sudo rm /etc/samba/smb.conf > /dev/null 2>&1;
+  sudo cat /tmp/smb.txt >> /etc/samba/smb.conf ;
+  sudo service smbd reload > /dev/null 2>&1;
   
  echo "Config SSH"
     cd /tmp > /dev/null 2>&1;
   wget https://raw.githubusercontent.com/Sirherobrine23/web-core/master/ssh.txt > /dev/null 2>&1;
-  rm /etc/sshd_config > /dev/null 2>&1;
-  cat /tmp/ssh.txt >> /etc/ssh/sshd_config ;
-  service ssh reload > /dev/null 2>&1;
-  service sshd reload > /dev/null 2>&1;
+  sudo rm /etc/sshd_config;
+  sudo cat /tmp/ssh.txt >> /etc/ssh/sshd_config ;
+  sudo service ssh reload > /dev/null 2>&1;
+  sudo service sshd reload > /dev/null 2>&1;
 clear;
 echo "Execute 'certboot --nginx' para terminar as configurações do nginx no HTTPS";
 echo "Adicione a porta 445 e 139 no firewall do sistema ou provedor(Azure, Google Cloud, AWS etc...)";
