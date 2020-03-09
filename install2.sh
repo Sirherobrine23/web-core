252e40d72d8c5300ca036eebfddc75087ae7ceb2
Matheus sampaio Queiroga <50121801+Sirherobrine23@users.noreply.github.com>
2020-03-09 01:08:33 -0300

    Ju

--- install2.sh
+++ install2.sh
@@ -12,36 +12,6 @@ for i in {1..20}; do
     echo -ne "\r${BAR:0:$i}" # print $i chars of $BAR from 0 position
     sleep .1                 # wait 100ms between "frames"
 done
-echo "Update Packages";
-  apt dist-upgrade -y > /dev/null 2>&1 ;
-  clear;
-  echo " Aguarde ";
-  BAR='####################'   # this is full bar, e.g. 20 chars
-
-for i in {1..20}; do
-    echo -ne "\r${BAR:0:$i}" # print $i chars of $BAR from 0 position
-    sleep .1                 # wait 100ms between "frames"
-done
-echo "Update Packages";
-  apt dist-upgrade -y > /dev/null 2>&1;
-  clear;
-  echo " Aguarde ";
-  BAR='####################'   # this is full bar, e.g. 20 chars
-
-for i in {1..20}; do
-    echo -ne "\r${BAR:0:$i}" # print $i chars of $BAR from 0 position
-    sleep .1                 # wait 100ms between "frames"
-done
-echo "Update Packages";
-  apt dist-upgrade -y > /dev/null 2>&1 ;
-  clear;
-  echo " Aguarde ";
-  BAR='####################'   # this is full bar, e.g. 20 chars
-
-for i in {1..20}; do
-    echo -ne "\r${BAR:0:$i}" # print $i chars of $BAR from 0 position
-    sleep .1                 # wait 100ms between "frames"
-done
 echo "Verificando a instalação do OPENSSH"
   apt install openssh-server -y > /dev/null 2>&1 ;
   clear;
